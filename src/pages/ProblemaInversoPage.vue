296d9b5752f132b5f5e081257ad980dedf511510
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-15 17:36:20 -0700

    chore: criacao da pagina Problema Inverso

---
+++ src/pages/ProblemaInversoPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>InversoPage</h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InversoPage'
+})
+</script>
